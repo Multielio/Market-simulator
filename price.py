a5324b99234cdba08d3b235e1de2116b1a7dcc04
Elio <8525139+Multielio@users.noreply.github.com>
2019-07-05 22:01:28 +0200

    New function that allows you to create your own dynamic borders functions

--- price.py
+++ price.py
@@ -53,7 +53,33 @@ def genPriceOverTime(numberOfPriceChanges, itemStartPrice, itemMaxPrice, itemMin
         mp.plot(np.linspace(0, numberOfPriceChanges-1, numberOfPriceChanges*10), continuousHighBorder(np.linspace(0, numberOfPriceChanges-1, numberOfPriceChanges*10)))
     return continuousPriceOverTime
 
+def advancedGenPriceOverTime(numberOfPriceChanges, itemStartPrice, itemMaxPrice, itemMinPrice, border, volatility, maxVol, f1, f2, visualize=False):
+    """
+    f1 and f2 should be periodic functions that oscillate between -1 and 1
+    """
+    t = np.arange(0,numberOfPriceChanges)
+    prices = [itemStartPrice]
+    minPriceBorder = [itemMinPrice]
+    maxPriceBorder = [itemMaxPrice]
+    currentPrice = itemStartPrice
+    for j in t[1:] : 
+        dynamicLowBorder = itemStartPrice -(itemStartPrice-itemMinPrice)*np.abs(f1(j))
+        dynamicHighBorder = itemStartPrice +(itemMaxPrice-itemStartPrice)*np.abs(f2(j))
+        currentPrice = genNextPrice(currentPrice, volatility, dynamicHighBorder, dynamicLowBorder, border, maxVol) # here I implemented a way to make maxprice and minprice fluctuate.
+        prices.append(currentPrice)
+        minPriceBorder.append(dynamicLowBorder)
+        maxPriceBorder.append(dynamicHighBorder)
+    continuousPriceOverTime = interp1d(t,prices)  
+    if visualize:
+        continuousLowBorder = interp1d(t,minPriceBorder) 
+        continuousHighBorder = interp1d(t,maxPriceBorder) 
+        mp.plot(np.linspace(0, numberOfPriceChanges-1, numberOfPriceChanges*10), continuousPriceOverTime(np.linspace(0, numberOfPriceChanges-1, numberOfPriceChanges*10)))  # just ploting to look at the curb
+        mp.plot(np.linspace(0, numberOfPriceChanges-1, numberOfPriceChanges*10), continuousLowBorder(np.linspace(0, numberOfPriceChanges-1, numberOfPriceChanges*10)))
+        mp.plot(np.linspace(0, numberOfPriceChanges-1, numberOfPriceChanges*10), continuousHighBorder(np.linspace(0, numberOfPriceChanges-1, numberOfPriceChanges*10)))
+    return continuousPriceOverTime
 
+
+#############################################################################################################################################################
 
 defaultItemStartPrice = 50  # Item default price
 defaultItemMaxPrice = defaultItemStartPrice*1.5
@@ -63,6 +89,11 @@ defaultVolatility = 0.01
 defaultMaxVol = 0.02  # Set the max volatility to avoid violent price moves
 
 
-continuousFunctionOfPricesOverTime =genPriceOverTime(10000,defaultItemStartPrice,defaultItemMaxPrice,defaultItemMinPrice,defaultBorder,defaultVolatility,defaultMaxVol,visualize=True)
+#continuousFunctionOfPricesOverTime =genPriceOverTime(10000,defaultItemStartPrice,defaultItemMaxPrice,defaultItemMinPrice,defaultBorder,defaultVolatility,defaultMaxVol,visualize=True)
+f1 = lambda  x : np.sin(x/300)
+f2 = lambda  x : np.sin(x*np.log(x))
+continuousFunctionOfPricesOverTime =advancedGenPriceOverTime(10000, defaultItemStartPrice, defaultItemMaxPrice, defaultItemMinPrice, defaultBorder, defaultVolatility, defaultMaxVol,f1, f2, visualize=True)
+
+
 
 # To evalute price at a given time use: continuousFunctionOfPricesOverTime(float).flat[0]
