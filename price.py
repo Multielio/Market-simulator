3aba0af36ae84020e62d5f4ddd544c510d6cbdea
Elio <8525139+Multielio@users.noreply.github.com>
2019-07-05 21:02:46 +0200

    Little rework of the code, variables names are more clear.

--- price.py
+++ price.py
@@ -8,14 +8,17 @@ import random
 import numpy as np
 import matplotlib.pyplot as mp
 from scipy.interpolate import interp1d
-init = 50 #Item default price
-maxprice = init*1.5
-minprice = init*0.5
-border = init*0.1 #Border arround maxprice or minprice where the price will want to go to the opposite direction
-volatility = 0.008 
-maxvol = 0.02 # set the max volatility to avoid violent price moves
 
-def gen(old_price):
+
+defaultItemStartPrice = 50  # Item default price
+defaultItemMaxPrice = defaultItemStartPrice*1.5
+defaultItemMinPrice = defaultItemStartPrice*0.5
+defaultBorder = defaultItemStartPrice*0.1  # Border arround maxprice or minprice where the price will want to go to the opposite direction
+defaultVolatility = 0.008 
+defaultMaxVol = 0.02  # Set the max volatility to avoid violent price moves
+
+
+def gen(old_price, volatility):
     rnd = random.random()
     change_percent = 2 * volatility * rnd
     if (change_percent > volatility):
@@ -23,27 +26,33 @@ def gen(old_price):
     change_amount = old_price * change_percent
     return old_price + change_amount
 
-def next_price(old_price, volatility, maxprice, minprice,border,maxvol):
-    g = gen(old_price)
+def genNextPrice(oldPrice, volatility, maxprice, minprice,border,maxvol):
+    g = gen(oldPrice, volatility)
     rand = random.random()
     while (g > maxprice-border*rand or g < minprice+border*rand):
         rand = random.random()
-        if maxvol>volatility:
+        if maxvol > volatility:
             volatility += 0.002
-        g = gen(old_price)
+        g = gen(oldPrice, volatility)
     volatility = 0.008
     return g	
 
 
 
+def genPriceOverTime(numberOfPriceChanges, itemStartPrice, itemMaxPrice, itemMinPrice, border, volatility, maxVol, visualize=False):
+    t = np.arange(0,numberOfPriceChanges)
+    prices = []
+    prices.append(itemStartPrice)
+    for j in t[1:] : 
+        itemStartPrice = genNextPrice(itemStartPrice,volatility, itemStartPrice +(itemMaxPrice-itemStartPrice)*np.abs(np.sin((1/700)*j)), itemStartPrice -(itemStartPrice-itemMinPrice)*np.abs(np.sin((1/300)*j)),border,maxVol) # here I implemented a way to make maxprice an minprice fluctuate.
+        prices.append(itemStartPrice)
+    
+    continuousPriceOverTime = interp1d(t,prices)  # z is a fonction that takes a integer and return the item price
+    
+    if visualize:
+        mp.plot(np.linspace(0,numberOfPriceChanges-1,numberOfPriceChanges*10),continuousPriceOverTime(np.linspace(0,numberOfPriceChanges-1,numberOfPriceChanges*10)))  # just ploting to look at the curb
+    return continuousPriceOverTime
 
-time = 700 # Amount of price changes wanted
-t = np.arange(0,time)
-l = []
-l.append(init)
-for j in t[1:] : 
-    init = next_price(init,volatility, init +(maxprice-init)*np.abs(np.sin((1/700)*j)), init -(init-minprice)*np.abs(np.sin((1/300)*j)),border,maxvol) # here I implemented a way to make maxprice an minprice fluctuate.
-    l.append(init)
+continuousFunctionOfPricesOverTime =genPriceOverTime(700,defaultItemStartPrice,defaultItemMaxPrice,defaultItemMinPrice,defaultBorder,defaultVolatility,defaultMaxVol,visualize=True)
 
-z = interp1d(t,l) # z is a fonction that takes a integer and return the item price
-mp.plot(np.linspace(0,time-1,time*10),z(np.linspace(0,time-1,time*10))) # just ploting to look at the curb
+# To evalute price at a given time use: continuousFunctionOfPricesOverTime(float).flat[0]
